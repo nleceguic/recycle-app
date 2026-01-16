34006960735472b27cf619ba09a2189ca02d987a
Noah Lecegui <nleceguic@gmail.com>
2026-01-16 16:26:56 +0100

    Add location permission handling to HomeActivity
    Introduces logic for requesting and handling location permissions in HomeActivity. Adds methods to obtain the user's current location and trigger nearby container detection, using FusedLocationProviderClient and ActivityResultContracts.

--- app/src/main/java/com/example/recycle/ui/home/HomeActivity.kt
+++ app/src/main/java/com/example/recycle/ui/home/HomeActivity.kt
@@ -1,10 +1,14 @@
 package com.example.recycle.ui.home
 
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.example.recycle.databinding.ActivityMainBinding
 import androidx.core.view.isVisible
@@ -13,16 +17,32 @@ import com.example.recycle.ui.map.MapActivity
 import com.example.recycle.ui.report.ReportActivity
 import androidx.activity.viewModels
 import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
 import com.example.recycle.data.model.FailureReason
 import com.example.recycle.data.model.WasteType
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val viewModel: HomeViewModel by viewModels()
+    private val locationPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+            val granted = permissions[Manifest.permission.ACCESS_FINE_LOCATION] == true
+            if (granted) {
+                getCurrentLocation()
+            }
+        }
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private var uLat: Double = 0.0
+    private var uLon: Double = 0.0
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -96,5 +116,31 @@ class HomeActivity : AppCompatActivity() {
         }
 
         showError(message)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    @SuppressLint("MissingPermission")
+    private fun getCurrentLocation() {
+        fusedLocationClient.getCurrentLocation(Priority.PRIORITY_HIGH_ACCURACY, null)
+            .addOnSuccessListener { location ->
+                if (location != null) {
+                    uLat = location.latitude
+                    uLon = location.longitude
+                    val accuracy = location.accuracy
+
+                    viewModel.detectNearbyContainers(userLatitude = uLat, userLongitude = uLon)
+                } else {
+                    showError("No se pudo obtener la ubicación")
+                }
+            }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun comprobarPermisoYLlamar() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            locationPermissionLauncher.launch(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION))
+        } else {
+            getCurrentLocation()
+        }
     }
 }
